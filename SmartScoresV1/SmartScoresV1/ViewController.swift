412d39a796f3ed9f33c89c102b48c1b50fb7951d
Ashly Lau <ashlylau@gmail.com>
2019-04-11 22:22:05 +0800

    created simple drawing app using tutorial

--- SmartScoresV1/SmartScoresV1/ViewController.swift
+++ SmartScoresV1/SmartScoresV1/ViewController.swift
@@ -8,27 +8,71 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    var lightOn = true
-    @IBAction func buttonPressed(_ sender: Any) {
-        lightOn = !lightOn
-        updateUI()
+class Canvas: UIView {
+    override func draw(_ rect: CGRect) {
+        //custom drawing
+        super.draw(rect)
+        
+        guard let context = UIGraphicsGetCurrentContext() else
+            { return }
+        
+        // here are my lines
+        // dummy data
+//        let startPoint = CGPoint(x: 0, y: 0)
+//        let endPoint = CGPoint(x: 100, y: 100)
+//
+//        context.move(to: startPoint)
+//        context.addLine(to: endPoint)
+        
+        context.setStrokeColor(UIColor.black.cgColor)
+        context.setLineWidth(5)
+        context.setLineCap(.butt)
+        
+        lines.forEach { (line) in
+            for (i, p) in line.enumerated() {
+                if i == 0 {
+                    context.move(to: p)
+                } else {
+                    context.addLine(to: p)
+                }
+            }
+        }
+        
+        context.strokePath()
     }
     
-    func updateUI() {
-        if lightOn {
-            view.backgroundColor = .white
-//            lightButton.setTitle("Off", for: .normal)
-        } else {
-            view.backgroundColor = .black
-//             lightButton.setTitle("On", for: .normal)
-        }
+    var lines = [[CGPoint]]()
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        lines.append([CGPoint]())
     }
+    
+    //track finger as we move across the screen
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let point = touches.first?.location(in: nil) else {
+            return
+        }
+//        print(point)
+        
+        guard var lastLine = lines.popLast() else { return }
+        lastLine.append(point)
+        
+        lines.append(lastLine)
+        
+        setNeedsDisplay()
+    }
+}
+
+class ViewController: UIViewController {
+    
+    let canvas = Canvas()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        updateUI()
+        
+        view.addSubview(canvas)
+        canvas.backgroundColor = .white
+        canvas.frame = view.frame
     }
 }
 
